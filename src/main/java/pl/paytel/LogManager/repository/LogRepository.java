bb0b758c1e36d112a8f2f10cce55b4fca0a8c6a2
Jakub Chmielowiec <jakub.chmielowiec@nexio.pl>
2020-05-31 18:36:29 +0200

    add mock repo

--- src/main/java/pl/paytel/LogManager/repository/LogRepository.java
+++ src/main/java/pl/paytel/LogManager/repository/LogRepository.java
@@ -1,5 +1,14 @@
 package pl.paytel.LogManager.repository;
 
+import pl.paytel.LogManager.model.Log;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface LogRepository {
+
+    Log save(Log log);
+
+    List<Log> getLogsByParameters(LocalDateTime dateFrom, LocalDateTime dateTo, Integer pageNumber, Integer pageSize);
 
 }
