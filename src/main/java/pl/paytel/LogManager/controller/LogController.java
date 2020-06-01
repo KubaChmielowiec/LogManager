cf0b7dad60f2076214ed550a580a2efd042f4e17
Jakub Chmielowiec <jakub.chmielowiec@nexio.pl>
2020-06-01 17:19:24 +0200

    change datetime format, consumes plain text

--- src/main/java/pl/paytel/LogManager/controller/LogController.java
+++ src/main/java/pl/paytel/LogManager/controller/LogController.java
@@ -3,6 +3,7 @@ package pl.paytel.LogManager.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -10,7 +11,6 @@ import pl.paytel.LogManager.model.Log;
 import pl.paytel.LogManager.service.LogService;
 
 import java.time.LocalDateTime;
-import java.util.IllegalFormatException;
 
 @RestController
 @RequestMapping("/log")
@@ -23,20 +23,18 @@ public class LogController {
         this.logService = logService;
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<Log> saveLog(@RequestBody String logString) {
         try {
             return new ResponseEntity<>(logService.saveLog(logString), HttpStatus.CREATED);
-        } catch (IllegalFormatException e) {
-            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Incorrect log format", e);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
         }
     }
 
     @GetMapping
-    public ResponseEntity<Iterable<Log>> getLogs(@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateFrom,
-                                             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTo,
+    public ResponseEntity<Iterable<Log>> getLogs(@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime dateFrom,
+                                             @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime dateTo,
                                              @RequestParam(required = false) Integer pageNumber,
                                              @RequestParam(required = false) Integer pageSize,
                                              @RequestParam(required = false) String resultFormat) {
